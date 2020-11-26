141d73a198e196d37e63eac2e91b0bb351b35981
Ramsteven <ramsteven@gmail.com>
2020-11-26 11:28:23 -0500

    fixing error built-in exit

--- auxiliar_functions_2.c
+++ auxiliar_functions_2.c
@@ -60,8 +60,14 @@ void chek_isatty(int check_line)
 		prompt();
 }
 
-
-void find_path (char **array, ssize_t *nread, bool *x)
+/**
+  *find_path- check if input is with pathline or keyboard
+  *@array: value change with function isatty
+  *@nread: counting characters of get_line
+  *@x: boolean controller
+  *Return: nothing
+  **/
+void find_path(char **array, ssize_t *nread, bool *x)
 {
 	int resta = 0;
 
@@ -76,43 +82,51 @@ void find_path (char **array, ssize_t *nread, bool *x)
 	}
 }
 
+/**
+  *call_built_in- check the input if is exit or env
+  *@array: value change with function isatty
+  *@stat_status: return of the main function
+  *@buffer: pointer that save the strings
+  *Return: boolean controler of check_stat
+  **/
+
 bool call_built_in(char **array, int *stat_status, char *buffer)
 {
-	int compare_env = 5;
-	int compare_exit = 5;
+	int compare_env = 5, compare_exit = 5;
 
+	compare_exit = _strcmp(array[0], "exit");
+	compare_env = _strcmp(array[0], "env");
 
-	compare_exit =_strcmp(array[0], "exit");
-
-	if (compare_exit == 0)
+	if (compare_env == 0 || compare_exit == 0)
 	{
-
-		*stat_status = 127;
-		free(buffer);
-		free(array);
-		exit(*stat_status);
-	}
-
-	compare_env =_strcmp(array[0], "env");
-
-	if(compare_env == 0)
-	{
-		int size_variable = 0;
-		int i = 0;
-
-
-	while(environ[i] != NULL)
+		if (compare_exit == 0)
 		{
-
-			size_variable = length(environ[i]);
-			write(STDOUT_FILENO,environ[i],size_variable);
-			write(STDOUT_FILENO,"\n",1);
-			i++;
+			free(buffer);
+			free(array);
+			exit(*stat_status);
 		}
+		if (compare_env == 0)
+		{
+			int size_variable = 0, i = 0;
 
+			while (environ[i] != NULL)
+			{
+				size_variable = length(environ[i]);
+				write(STDOUT_FILENO, environ[i], size_variable);
+				write(STDOUT_FILENO, "\n", 1);
+				i++;
+			}
+		}
+		else
+		{
+			write(STDOUT_FILENO, array[0], length(array[0]));
+			write(STDOUT_FILENO, ": ‘", 3);
+			write(STDOUT_FILENO, array[1], length(array[1]));
+			write(STDOUT_FILENO, "’ : No such file or directory", 29);
+			*stat_status = 127;
+			return (false);
+		}
+		return (true);
 	}
-	else
-		return (false);
-
-	return (true);
+	return (false);
 }
