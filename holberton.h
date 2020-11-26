19563aa9564d8388e2636aacb870fd57c26eb036
Ramsteven <ramsteven@gmail.com>
2020-11-25 19:12:52 -0500

    fixing problems with header file

--- holberton.h
+++ holberton.h
@@ -22,19 +22,19 @@ void manage_error(char *cont_array, ssize_t nread, int counter_procces);
 /*check commnad*/
 void handler(int num);
 void prompt(void);
-void check_stat(char **array, ssize_t nread, char * buffer, int *stat_status);
+void check_stat(char **array, ssize_t nread, char *buffer, int *stat_status);
 ssize_t check_get_line(char **array, int *stat_status);
 
 
 /*auxiliar functions*/
 ssize_t length(char *size);
-char* _strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src);
 int _strcmp(char *a, char *b);
 char *_itoa(int num, char *strnum);
 char *_strcpy(char *dest, char *src);
 
 /*auxiliar functions*/
-void fix_token (char * token);
+void fix_token(char *token);
 void *_calloc(unsigned int nmemb, unsigned int size);
 void chek_isatty(int check_line);
 void _exit_built(char *array_0, char *buffer, int *stat_status);
@@ -45,9 +45,9 @@ void check_o(int i, int check_line, const char *delim, bool x);
 
 
 /*which*/
-void functionToken_which(char * dest, char **pathDivision);
+void functionToken_which(char *dest, char **pathDivision);
 char *_strcpy_which_path(char *dest, char *src);
-char *_which (char **array);
+char *_which(char **array);
 
 
-#endif /*Holberton.h*/
+#endif
